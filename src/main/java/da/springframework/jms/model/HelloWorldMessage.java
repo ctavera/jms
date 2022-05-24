31fb97ce59d6801c18f8053bfca2b1fda5f4d9de
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-05-24 17:52:20 -0500

    174. Java Message Object.

---
+++ src/main/java/da/springframework/jms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package da.springframework.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 4322767514154450107L;
+
+    private UUID id;
+    private String message;
+}
